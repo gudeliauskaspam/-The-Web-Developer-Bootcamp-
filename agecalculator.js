99c86ce9f3cd828bf2966c53a9bfd9c8d7d586a9
Pâmela Gudeliauskas <47401012+gudeliauskaspam@users.noreply.github.com>
2019-08-09 17:16:50 -0300

    Add files via upload

---
+++ agecalculator.js
@@ -0,0 +1,4 @@
+var age = prompt("What's your age? ");
+var days = age * 365
+
+alert("You have been alive for: " + days + " days.");
---
+++ jstalkerexercise.js
@@ -0,0 +1,6 @@
+var first_name = prompt("What's your 1st name?");
+var last_name = prompt("What's your last name?");
+var age = prompt("How old are you?");
+
+alert("Your name is: " + first_name + " " + last_name);
+alert("Your age is: " + age);
